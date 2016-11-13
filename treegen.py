c378897e38fab1ff70a4f7350296fe911abe00bc
ikit <olivier@labsquare.org>
2016-11-13 15:08:38 +0100

    tree draw tree wireframe

--- treegen.py
+++ treegen.py
@@ -1,12 +1,17 @@
+#!python
+# coding: utf-8
+
 import os
+import sys
 import operator
 import math
+import random
+import pygame
+
 from math import acos
 from math import sqrt
 from math import pi
-import random
-##import pygame
-##from pygame.locals import *
+from pygame.locals import *
 
 def meth_brch_normal(vect_brch):
     return vect_brch
@@ -15,7 +20,7 @@ def meth_rand_aleat(a_rand):
 
 
 # les variables globales utilisées
-embrchmt_min = 1
+embrchmt_min = 2
 embrchmt_max = 3
 tropisme     = (2 * pi) / 3.0
 rap_brch     = 0.7
@@ -56,6 +61,9 @@ def deg2rad(angle):
 def s_v (scalaire, vecteur) :
     return (vecteur[0] * scalaire, vecteur[1] * scalaire)
 
+def v_add(v1, v2):
+    return (v1[0] + v2[0], v1[1] + v2[1])
+
 def rotate(angle, vecteur):
     cs = math.cos(angle)
     sn = math.sin(angle)
@@ -91,60 +99,89 @@ def gener_embrch(brch_n):
 
 
 # genere l'arbre d'ordre n en utilisant gener_embrch_n+1
-def gener_squelette(ordre, sqlt):
+def gener_squelette(ordre, root):
     if ordre == 0:
-        return [sqlt]
-    embrchmt = gener_embrch (sqlt)
-    arbre = [b for lb in embrchmt for b in gener_squelette(ordre-1, lb) ]
-    return [sqlt] + arbre
+        return (root, [])
+    embrchmt = gener_embrch (root)
+
+    arbre = [gener_squelette(ordre-1, b) for b in embrchmt]
+    return (root, arbre)
 
 
 
 
 
-print( gener_squelette(0, (0,1)) )
-print( gener_squelette(1, (0,1)) )
-print( gener_squelette(2, (0,1)) )
-print( gener_squelette(3, (0,1)) )
+# print( gener_squelette(0, (0,1)) )
+# print( gener_squelette(1, (0,1)) )
+# print( gener_squelette(2, (0,1)) )
+# print( gener_squelette(3, (0,1)) )
 
 
 
 
 
+# convertit le sqelette pour tracer l'arbre en fil de fer
+def sqlt2coord_fdfer(squelette, point):
+    if len(squelette) == 0: 
+        return []
+
+
+    point_suivant = v_add(point, squelette[0])
+        
+    res = [(point, point_suivant)]
+    for b in squelette[1]:
+        res += sqlt2coord_fdfer(b, point_suivant)
+    return res
+
+
+  
+pygame.init()
+  
+# set up the window
+DISPLAYSURF = pygame.display.set_mode((800, 800), 0, 32)
+pygame.display.set_caption('Drawing')
+  
+# set up the colors
+BLACK = (  0,   0,   0)
+WHITE = (255, 255, 255)
+RED   = (255,   0,   0)
+GREEN = (  0, 255,   0)
+BLUE  = (  0,   0, 255)
+  
+# draw on the surface object
+DISPLAYSURF.fill(BLACK)
+#pygame.draw.polygon(DISPLAYSURF, GREEN, ((146, 0), (291, 106), (236, 277), (56, 277), (0, 106)))
+#pygame.draw.line(DISPLAYSURF, BLUE, (60, 60), (120, 60), 4)
+#pygame.draw.line(DISPLAYSURF, BLUE, (120, 60), (60, 120))
+#pygame.draw.line(DISPLAYSURF, BLUE, (60, 120), (120, 120), 4)
+#pygame.draw.circle(DISPLAYSURF, BLUE, (300, 50), 20, 0)
+#pygame.draw.ellipse(DISPLAYSURF, RED, (300, 200, 40, 80), 1)
+#pygame.draw.rect(DISPLAYSURF, RED, (200, 150, 100, 50))
+  
+#pixObj = pygame.PixelArray(DISPLAYSURF)
+#pixObj[380][280] = BLACK
+#pixObj[382][282] = BLACK
+#pixObj[384][284] = BLACK
+#pixObj[386][286] = BLACK
+#pixObj[388][288] = BLACK
+#del pixObj
 
 
 
+arbre = gener_squelette(4, (0,100))
+print(arbre)
+segments = sqlt2coord_fdfer(arbre, (400, 400))
+print(segments)
+
+for s in segments:
+    pygame.draw.line(DISPLAYSURF, WHITE, s[0], s[1])
+
+# run the game loop
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
 
 
-
-
-
-
-
-
-
-
-
-
-##pygame.init()
-##
-###Ouverture de la fenêtre Pygame
-##fenetre = pygame.display.set_mode((640, 480))
-##
-###Chargement et collage du fond
-##fond = pygame.image.load("C:/Users/Olive/Desktop/background.jpg").convert()
-##fenetre.blit(fond, (0,0))
-##
-###Chargement et collage du personnage
-##perso = pygame.image.load("C:/Users/Olive/Desktop/perso.png").convert_alpha()
-##fenetre.blit(perso, (200,300))
-##
-###Rafraîchissement de l'écran
-##pygame.display.flip()
-##
-###BOUCLE INFINIE
-##continuer = 1
-##while continuer:
-##	for event in pygame.event.get():
-##		if event.type == QUIT:
-##			continuer = 0
